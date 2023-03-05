4d47d5ab9a0878b7b5a2d5d99c532643865ab3e8
Dogan Mus <doganmus87@gmail.com>
2023-03-05 13:52:22 +0300

    eğitim

---
+++ JS/message.js
@@ -0,0 +1,13 @@
+function Call() {
+    alert("Beni hangi fonksiyon çağırdı.");
+}
+
+function GoodBye() {
+    alert("Güle Güle");
+}
+
+function Welcome() {
+    alert("Hoş Geldin JS");
+}
+//harici komut dosyalalrı içerisine script yazılmaz.
+//harici komut dosyaları  karışıklığı azaltır.
